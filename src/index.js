030d2ed64f89e5f85028a9dfb740cd872d7df9fa
Guilherme <guilhermet213@gmail.com>
2022-04-28 22:18:51 -0300

    feat: basic api implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import Router from '@koa/router';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.use(bodyParser());
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
