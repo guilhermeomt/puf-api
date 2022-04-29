33e9ca50fb35a9f0731d3e8ab6ace1f1e14776a5
Guilherme <guilhermet213@gmail.com>
2022-04-29 01:09:50 -0300

    feat: users route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
-import Router from '@koa/router';
+
+import { router } from './routes';
 
 const app = new Koa();
-const router = new Router();
-
-router.get('/', ctx => {
-  ctx.body = 'Hello World';
-});
 
 app.use(bodyParser());
 
