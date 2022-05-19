4270be7dec6815a5ad507968098cb2fd7e188108
Guilherme <guilhermet213@gmail.com>
2022-05-18 23:51:38 -0300

    test: return logged in user by correct credentials

--- src/routes.spec.js
+++ src/routes.spec.js
@@ -1,9 +1,34 @@
 import request from 'supertest';
+import bcrypt from 'bcrypt';
+import { prisma } from '~/data';
+
 import { app } from './app';
 
 const server = app.listen();
 
+const name = 'User';
+const email = 'ola@guilhermeomt.dev';
+const password = '123456';
+
 describe('Users routes', () => {
+  beforeAll(async () => {
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+  });
+
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+    server.close();
+  });
+
   it('should return not found with wrong password', async () => {
     const email = 'ola@guilhermeomt.dev';
     const password = '123';
@@ -23,8 +48,18 @@ describe('Users routes', () => {
       .get('/login')
       .auth(email, password, { type: 'basic' });
 
-    console.log(response);
-
     expect(response.status).toBe(404);
+  });
+
+  it('should return logged in user by correct credentials', async () => {
+    const response = await request(server)
+      .get('/login')
+      .auth(email, password, { type: 'basic' });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeTruthy();
+    expect(response.body.user.id).toBeTruthy();
+    expect(response.body.user.email).toBe(email);
+    expect(response.body.user.password).toBeFalsy();
   });
 });
